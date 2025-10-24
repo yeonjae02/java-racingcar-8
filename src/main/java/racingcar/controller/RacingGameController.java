99fb100ba8f91531a2f85735b797ee79c03b86ed
yeonjae02 <0yeonjae2@naver.com>
2025-10-24 21:33:32 +0900

    feat(controller): 게임 실행 및 결과 출력 로직 구현

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -1,8 +1,12 @@
 package racingcar.controller;
 
+import java.util.ArrayList;
+import racingcar.domain.Car;
+import racingcar.domain.RacingCarGame;
 import racingcar.validation.RacingCarInputChecker;
 import racingcar.view.RacingCarInput;
 import java.util.List;
+import racingcar.view.RacingCarOutput;
 
 public class RacingGameController {
     private final RacingCarInputChecker checker;
@@ -14,6 +18,8 @@ public class RacingGameController {
     public void run() {
         List<String> carNames = getValidatedNames();
         int playCount = getValidatedPlayCount();
+        RacingCarGame game = createGame(carNames);
+        runGame(game, playCount);
     }
 
     private List<String> getValidatedNames() {
@@ -26,4 +32,21 @@ public class RacingGameController {
         return checker.getPlayCount(countInput);
     }
 
+    private RacingCarGame createGame(List<String> carNames) {
+        List<Car> cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name, 0));
+        }
+        return new RacingCarGame(cars);
+    }
+
+    private static void runGame(RacingCarGame game, int playCount) {
+        System.out.println("\n실행 결과");
+        for (int i = 0; i < playCount; i++) {
+            game.playRound();
+            RacingCarOutput.printRoundResult(game.getCars());
+            System.out.println();
+        }
+        RacingCarOutput.printWinners(game.getWinners());
+    }
 }
