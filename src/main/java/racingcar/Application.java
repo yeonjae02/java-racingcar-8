7ca644f20b6a953957da386944c676fce6982e5d
yeonjae02 <0yeonjae2@naver.com>
2025-10-24 21:02:26 +0900

    feat(app): 메인 실행부 및 예외 처리 로직 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingGameController controller = new RacingGameController();
+
+        try {
+            controller.run();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
