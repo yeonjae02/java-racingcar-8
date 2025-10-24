f88e3869343952e79b00aef9204d5fc0a44f0110
yeonjae02 <0yeonjae2@naver.com>
2025-10-24 21:33:54 +0900

    fix(app): 예외가 전파되도록 try-catch 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,11 +6,6 @@ public class Application {
 
     public static void main(String[] args) {
         RacingGameController controller = new RacingGameController();
-
-        try {
-            controller.run();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        controller.run();
     }
 }
